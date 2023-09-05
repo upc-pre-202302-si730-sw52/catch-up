72fbbc61939ea5d0b655d861be760b020864fb86
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-04 20:42:47 -0500

    feat(ui): added primevue component library and theme.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+// App Theme
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+// Add PrimeFlex
+import 'primeflex/primeflex.css';
+
+// Add PrimeVue Components
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import SelectButton from 'primevue/selectbutton';
+import Sidebar from 'primevue/sidebar';
+import Avatar from 'primevue/avatar';
+import Menu from 'primevue/menu';
+import MenuBar from 'primevue/menubar';
+import Toolbar from 'primevue/toolbar';
+
+
+createApp(App)
+    .use(PrimeVue, { ripple: true })
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', MenuBar)
+    .component('pv-toolbar', Toolbar)
+    .mount('#app')
