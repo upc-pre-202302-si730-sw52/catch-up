2ef3e5cf9561a9dbdb749c60c0a527403eb016de
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-06 20:16:32 -0500

    feat(side-menu): added side menu with available sources.

---
+++ src/news/components/side-menu.component.vue
@@ -0,0 +1,59 @@
+<template>
+  <pv-sidebar v-bind:visible="visible">
+    <div v-for="source in sources" class="m-4">
+      <div @click="onSourceSelected(source)"
+           class="flex align-content-start flex-wrap">
+        <span class="flex align-items-center justify-content-center mr-2">
+          <pv-avatar :image="source.urlToLogo"
+                     :aria-label="source.name"
+                     shape="circle"/>
+        </span>
+        <span class="flex align-items-center justify-content-center">
+          {{ source.name }}
+        </span>
+      </div>
+    </div>
+  </pv-sidebar>
+</template>
+
+<script>
+import {NewsApiService} from "../services/news-api.service.js";
+
+export default {
+  name: "side-menu",
+  props: {
+    visible: Boolean
+  },
+  data() {
+    return {
+      sources: [],
+      errors: [],
+      newsApi: new NewsApiService()
+    }
+  },
+  created() {
+    this.newsApi.getSources()
+        .then(response => {
+          this.sources = response.data.sources;
+          this.sources.forEach(source => {
+            source.urlToLogo = this.newsApi.getUrlToLogo(source);
+            console.log('data: ');
+            console.log(response.data.sources);
+          });
+        })
+        .catch(e => this.errors.push(e));
+  },
+  methods: {
+    onSourceSelected(source) {
+      this.$emit('source-selected', source);
+    },
+    isVisible() {
+      return this.visible;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
