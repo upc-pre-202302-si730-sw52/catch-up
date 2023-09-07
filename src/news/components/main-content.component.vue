2273cecb3699347e14670e44723ba9742f876679
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-06 19:59:32 -0500

    feat(main-content): added main content component.

---
+++ src/news/components/main-content.component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-for="article in articles">
+    <article-card :article="article"></article-card>
+  </div>
+</template>
+
+<script>
+import ArticleCard from "./article-card.component.vue";
+
+export default {
+  name: "main-content",
+  components: {ArticleCard},
+
+  props: {
+    articles: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
